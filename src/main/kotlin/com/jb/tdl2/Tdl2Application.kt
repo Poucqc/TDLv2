463292f28b8a8e5771cbe39c97992dcd300f5ae9
pcouqc <nagapen@nate.com>
2024-06-21 16:59:26 +0900

    create : build project

---
+++ src/main/kotlin/com/jb/tdl2/Tdl2Application.kt
@@ -0,0 +1,11 @@
+package com.jb.tdl2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Tdl2Application
+
+fun main(args: Array<String>) {
+    runApplication<Tdl2Application>(*args)
+}
---
+++ src/test/kotlin/com/jb/tdl2/Tdl2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jb.tdl2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Tdl2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
