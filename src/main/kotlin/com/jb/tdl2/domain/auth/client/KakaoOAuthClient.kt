cc08587903d86af2693184eea8e81a2bca6eb3c8
pcouqc <nagapen@nate.com>
2024-06-25 16:48:42 +0900

    feat : implement social login

--- src/main/kotlin/com/jb/tdl2/domain/auth/client/KakaoOAuthClient.kt
+++ src/main/kotlin/com/jb/tdl2/domain/auth/client/KakaoOAuthClient.kt
@@ -3,20 +3,59 @@ package com.jb.tdl2.domain.auth.client
 import com.jb.tdl2.domain.auth.dto.KakaoUserInfoResponse
 import com.jb.tdl2.domain.auth.dto.TokenResponse
 import com.jb.tdl2.domain.auth.dto.UserInfoResponse
+import com.jb.tdl2.domain.auth.properties.kakao.KakaoConfig
+import com.jb.tdl2.domain.exception.InvalidTokenException
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
 
 @Component(value = "kakaoOAuthClient")
-class KakaoOAuthClient:OAuthClient{
+class KakaoOAuthClient(
+    private val kakaoConfig: KakaoConfig,
+    private val restClient: RestClient
+) : OAuthClient {
     override fun generateLoginPage(): String {
-        TODO("Not yet implemented")
+        return StringBuilder(kakaoConfig.authServerBaseUrl)
+            .append("/oauth/authorize")
+            .append("?client_id=").append(kakaoConfig.clientId)
+            .append("&redirect_uri=").append(kakaoConfig.redirectUrl)
+            .append("&response_type=").append("code")
+            .toString()
     }
 
     override fun getAccessToken(code: String): String {
-        TODO("Not yet implemented")
+        val requestData = mutableMapOf(
+            "grant_type" to "authorization_code",
+            "client_id" to kakaoConfig.clientId,
+            "code" to code
+        )
+        return restClient.post()
+            .uri("${kakaoConfig.authServerBaseUrl}/oauth/token")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(LinkedMultiValueMap<String, String>().apply { this.setAll(requestData) })
+            .retrieve()
+            .onStatus(HttpStatusCode::isError) { _, _ ->
+                throw InvalidTokenException("kakao")
+            }
+            .body<TokenResponse>()
+            ?.accessToken
+            ?: throw InvalidTokenException("kakao")
     }
 
     override fun retrieveUserInfo(token: String): KakaoUserInfoResponse {
-        TODO("Not yet implemented")
+        return restClient.get()
+            .uri("${kakaoConfig.authServerBaseUrl}/v2/user/me")
+            .header("Authorization", "Bearer $token")
+            .retrieve()
+            .onStatus(HttpStatusCode::isError) { _, _ ->
+                throw InvalidTokenException("kakao")
+            }
+            .body<KakaoUserInfoResponse>()
+            ?: throw InvalidTokenException("kakao")
+    }
     }
 
 }
