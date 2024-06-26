2a9c92f257b91eeb4def396be8f6e588430578fa
pcouqc <nagapen@nate.com>
2024-06-26 21:30:16 +0900

    feat : add val for response

--- src/main/kotlin/com/jb/tdl2/domain/user/dto/ProfileResponse.kt
+++ src/main/kotlin/com/jb/tdl2/domain/user/dto/ProfileResponse.kt
@@ -1,14 +1,19 @@
 package com.jb.tdl2.domain.user.dto
 
 import com.jb.tdl2.domain.user.model.User
+import java.util.Date
 
 data class ProfileResponse(
     val nickname: String,
+    val joinDate: Date,
+    val isBanned: Boolean,
 ) {
     companion object {
         fun from(user: User): ProfileResponse {
             return ProfileResponse(
                 nickname = user.nickname,
+                joinDate = user.joinDate,
+                isBanned = user.isBanned,
             )
         }
     }
