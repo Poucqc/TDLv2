a9166f8a2cb3e989575e2e5d264df5d2f06bbd57
pcouqc <nagapen@nate.com>
2024-06-26 21:22:36 +0900

    fix : change val type

--- src/main/kotlin/com/jb/tdl2/domain/auth/dto/NaverUserInfoResponse.kt
+++ src/main/kotlin/com/jb/tdl2/domain/auth/dto/NaverUserInfoResponse.kt
@@ -1,7 +1,7 @@
 package com.jb.tdl2.domain.auth.dto
 
 data class NaverUserInfoResponse(
-    override val id: Long,
+    override val id: String,
     override val email: String,
     override val nickname: String,
 ) : UserInfoResponse {
--- src/main/kotlin/com/jb/tdl2/domain/auth/dto/UserInfoResponse.kt
+++ src/main/kotlin/com/jb/tdl2/domain/auth/dto/UserInfoResponse.kt
@@ -1,7 +1,7 @@
 package com.jb.tdl2.domain.auth.dto
 
 interface UserInfoResponse {
-    val id: Long
+    val id: String
     val email: String
     val nickname: String
 }
