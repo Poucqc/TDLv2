89cbff4c7358008551bbb73ddfe0b60d655a334a
pcouqc <nagapen@nate.com>
2024-06-26 21:24:06 +0900

    feat : impl redis config and class

---
+++ src/main/kotlin/com/jb/tdl2/domain/common/redis/RedisConfig.kt
@@ -0,0 +1,18 @@
+package com.jb.tdl2.domain.common.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.StringRedisTemplate
+
+@Configuration
+class RedisConfig {
+
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
+        val template = StringRedisTemplate()
+        template.connectionFactory = connectionFactory
+        return template
+    }
+}
