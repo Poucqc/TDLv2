7da542b7601ae2822c0499a8f007e5b2c19b0c4b
pcouqc <nagapen@nate.com>
2024-06-26 21:25:35 +0900

    feat : add, change methods

--- src/main/kotlin/com/jb/tdl2/domain/user/service/UserService.kt
+++ src/main/kotlin/com/jb/tdl2/domain/user/service/UserService.kt
@@ -1,24 +1,31 @@
 package com.jb.tdl2.domain.user.service
 
+import com.jb.tdl2.domain.auth.dto.UserInfoResponse
 import com.jb.tdl2.domain.user.dto.*
 import org.springframework.stereotype.Service
 
 @Service
 interface UserService {
-    fun login(request: LoginRequest): LoginResponse?
+    fun login(request: LoginRequest): LoginResponse
 
-    fun register(request: RegisterRequest): UserResponse?
+    fun register(request: RegisterRequest): UserResponse
 
-    fun getProfile(userId: Long): ProfileResponse?
+    fun verifyEmail(currentId: Long,verifyCode: String): UserResponse
 
-    fun profileUpdate(password: String, request: ProfileUpdateRequest, currentId: Long): UserResponse?
+    fun registerUserIfAbsent(userInfo: UserInfoResponse, provider: String): UserResponse
 
-    fun unregister(userId: Long, password: String)
+    fun getProfile(userId: Long): ProfileResponse
 
-    fun followUser(userId: Long, currentId: Long): FollowResponse?
+    fun profileUpdate(request: ProfileUpdateRequest, currentId: Long): UserResponse
 
-    fun unfollowUser(userId: Long, currentId: Long): FollowResponse?
+    fun passwordChange(request: PasswordChangeRequest, currentId: Long): UserResponse
 
-    fun reportUser(userId: Long, currentId: Long)
+    fun unregister(currentId: Long, password: String)
+
+    fun followUser(userId: Long, currentId: Long): FollowResponse
+
+    fun unfollowUser(userId: Long, currentId: Long): FollowResponse
+
+    fun reportUser(userId: Long, currentId: Long): ReportResponse
 
 }
