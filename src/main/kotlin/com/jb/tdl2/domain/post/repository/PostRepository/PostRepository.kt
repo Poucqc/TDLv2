8a40abd2a2cfe4c5b61cc41b4496f850d0952be6
pcouqc <nagapen@nate.com>
2024-06-27 02:04:43 +0900

    refactor : remove unused import

--- src/main/kotlin/com/jb/tdl2/domain/post/repository/PostRepository/PostRepository.kt
+++ src/main/kotlin/com/jb/tdl2/domain/post/repository/PostRepository/PostRepository.kt
@@ -2,7 +2,6 @@ package com.jb.tdl2.domain.post.repository.PostRepository
 
 import com.jb.tdl2.domain.post.model.Post
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
 interface PostRepository: JpaRepository<Post, Long>, CustomPostRepository {
 }
