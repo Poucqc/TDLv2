b1e5ed7d9981a703dc60686f3272f94c0910343d
pcouqc <nagapen@nate.com>
2024-06-28 22:41:30 +0900

    feat : build base mvc for comment

--- src/main/kotlin/com/jb/tdl2/domain/comment/model/Comment.kt
+++ src/main/kotlin/com/jb/tdl2/domain/comment/model/Comment.kt
@@ -1,19 +1,43 @@
 package com.jb.tdl2.domain.comment.model
 
+import com.jb.tdl2.domain.comment.dto.CommentResponse
+import com.jb.tdl2.domain.post.model.Post
 import com.jb.tdl2.domain.user.model.User
 import jakarta.persistence.*
+import java.time.LocalDateTime
 
 
 @Entity
 @Table(name = "comments")
 class Comment(
 
-    var content: String,
+    @Column(name = "conents")
+    var body: String,
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime,
+
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime,
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    val user: User
+    val user: User,
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    val post: Post,
 ) {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
+
+    fun toResponse(): CommentResponse {
+        return CommentResponse(
+            id = this.id!!,
+            authorId = this.user.id!!,
+            authorName = this.user.nickname,
+            body = this.body,
+            updatedAt = this.updatedAt
+        )
+    }
 }
---
+++ src/main/kotlin/com/jb/tdl2/domain/comment/service/CommentService.kt
@@ -0,0 +1,7 @@
+package com.jb.tdl2.domain.comment.service
+
+import org.springframework.stereotype.Service
+
+@Service
+interface CommentService {
+}
