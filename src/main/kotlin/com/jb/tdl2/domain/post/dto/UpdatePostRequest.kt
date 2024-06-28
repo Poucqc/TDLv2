7ee49dd1c9f5e1be7f6b47183191e29a6fc9665b
pcouqc <nagapen@nate.com>
2024-06-28 22:37:48 +0900

    feat : add annotation for valid

--- src/main/kotlin/com/jb/tdl2/domain/post/dto/UpdatePostRequest.kt
+++ src/main/kotlin/com/jb/tdl2/domain/post/dto/UpdatePostRequest.kt
@@ -1,7 +1,15 @@
 package com.jb.tdl2.domain.post.dto
 
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+
 data class UpdatePostRequest(
+
+    @field:Size(min = 1, max = 50, message = "제목을 1~50 글자 이내로 입력해 주세요")
     val title: String,
+    @field:Size(min = 1, max = 2000, message = "본문을 1~2000 글자 이내로 입력해 주세요")
     val content: String,
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]*$", message = "영어, 한글, 숫자만 포함되어야 합니다")
+    @field:Size(min =1, max = 12, message = "해쉬태그는 1~12 글자 이내여야 합니다")
     val hashtag: Set<String>
 )
