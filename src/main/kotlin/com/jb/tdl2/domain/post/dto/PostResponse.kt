dc5a31d3020ad47a1e63f70f26b39c9e973d113d
pcouqc <nagapen@nate.com>
2024-06-28 22:39:17 +0900

    feat : retype response vals

--- src/main/kotlin/com/jb/tdl2/domain/post/dto/PostResponse.kt
+++ src/main/kotlin/com/jb/tdl2/domain/post/dto/PostResponse.kt
@@ -1,7 +1,9 @@
 package com.jb.tdl2.domain.post.dto
 
+import com.jb.tdl2.domain.comment.dto.CommentResponse
 import com.jb.tdl2.domain.comment.model.Comment
 import com.jb.tdl2.domain.post.model.Hashtag
+import com.jb.tdl2.domain.post.model.Post
 import java.time.LocalDateTime
 
 data class PostResponse(
@@ -9,9 +11,10 @@ data class PostResponse(
     val title: String,
     val content: String,
     val updatedAt: LocalDateTime,
-    val hashtag: Hashtag,
-    val comments: List<Comment>,
+    val hashtag: MutableSet<Hashtag>,
+    val comments: List<CommentResponse>,
     val authorId: Long,
     val authorName: String,
     val likesCount: Int,
+    val viewCount: Int,
 )
