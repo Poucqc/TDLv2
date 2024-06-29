ae48663262384cc39d1aad6a735f685fb8b51889
pcouqc <nagapen@nate.com>
2024-06-29 23:02:53 +0900

    feat : add repositories for post's fields

--- src/main/kotlin/com/jb/tdl2/domain/post/repository/LikeRepository.kt
+++ src/main/kotlin/com/jb/tdl2/domain/post/repository/LikeRepository.kt
@@ -4,7 +4,9 @@ import com.jb.tdl2.domain.post.model.Like
 import com.jb.tdl2.domain.post.model.LikeId
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface LikeRepository : JpaRepository<Like, LikeId> {
 
     @Query("select count(l) from Like l where l.id.postId = :postId")
--- src/main/kotlin/com/jb/tdl2/domain/post/repository/PostHashtagRepository.kt
+++ src/main/kotlin/com/jb/tdl2/domain/post/repository/PostHashtagRepository.kt
@@ -1,11 +1,9 @@
 package com.jb.tdl2.domain.post.repository
 
-import com.jb.tdl2.domain.post.model.Hashtag
+import com.jb.tdl2.domain.post.model.PostHashtag
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface HashtagRepository: JpaRepository<Hashtag, Long> {
-
-    fun findByNameIn(names: Set<String>): Set<Hashtag>
+interface PostHashtagRepository : JpaRepository<PostHashtag, Long> {
 }
