65eb8be8c7a3b4a052a59d8a8cb82d1f69396df4
pcouqc <nagapen@nate.com>
2024-06-30 02:55:48 +0900

    feat : add methods for service

--- src/main/kotlin/com/jb/tdl2/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/jb/tdl2/domain/comment/service/CommentService.kt
@@ -1,7 +1,16 @@
 package com.jb.tdl2.domain.comment.service
 
+import com.jb.tdl2.domain.comment.dto.CommentResponse
+import com.jb.tdl2.domain.comment.dto.UpdateCommentRequest
 import org.springframework.stereotype.Service
 
 @Service
 interface CommentService {
+
+    fun addComment(currentId: Long, request: UpdateCommentRequest, postId: Long): CommentResponse
+
+    fun updateComment(currentId: Long, request: UpdateCommentRequest, postId: Long, commentId: Long): CommentResponse
+
+    fun deleteComment(currentId: Long, postId: Long, commentId: Long)
+
 }
