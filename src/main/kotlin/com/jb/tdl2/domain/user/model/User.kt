d2e4d195f0e0e23bb26bb3579c62a33d228380e5
pcouqc <nagapen@nate.com>
2024-06-29 23:38:31 +0900

    refactor : var to private for reliability
    - ensure private var in entity to prevent direct use of getter()&setter()
    - use val in entity when applicable

--- src/main/kotlin/com/jb/tdl2/domain/user/model/User.kt
+++ src/main/kotlin/com/jb/tdl2/domain/user/model/User.kt
@@ -1,5 +1,9 @@
 package com.jb.tdl2.domain.user.model
 
+import com.jb.tdl2.domain.exception.NoPermissionException
+import com.jb.tdl2.domain.exception.NotMatchException
+import com.jb.tdl2.security.PasswordUtils
+import com.jb.tdl2.security.PasswordUtils.hashPassword
 import jakarta.persistence.*
 import java.util.*
 
@@ -8,34 +12,78 @@ import java.util.*
 class User(
 
     @Column(unique = true, nullable = false, name = "email")
-    val email: String,
+    private val email: String,
 
     @Column(unique = true, nullable = false, name = "user_name")
-    var nickname: String,
+    private var nickname: String,
 
     @Column(name = "provider")
-    val provider: String?,
+    private val provider: String?,
 
     @Column(name = "provider_id")
-    val providerId: String?,
+    private val providerId: String?,
 
     @Column(nullable = false, name = "joined_date")
-    val joinDate: Date,
+    private val joinDate: Date,
 
     @Column(name = "hashed_password")
-    var password: String?,
+    private var password: String?,
 
     @Column(nullable = false, name = "ban_status")
-    var isBanned: Boolean = false,
+    private var isBanned: Boolean = false,
 
     @Column(nullable = false, name = "delete_status")
-    var deleted: Boolean = false,
+    private var isDeleted: Boolean = false,
 
     @Column(name = "deleted_at")
-    var deletedDate: Date? = null,
+    private var deletedDate: Date? = null,
 
     ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
+
+    fun validOAuthLoginUser() {
+        if(this.provider != null) {
+            throw NoPermissionException("login to social login user")
+        }
+    }
+
+    fun validDeletedUser() {
+        if(this.isDeleted) {
+            throw NoPermissionException("deleted user")
+        }
+    }
+
+    fun verifyPassword(password: String, message: String) {
+        if (!PasswordUtils.verifyPassword(password, this.password!!)) {
+            throw NotMatchException(message)
+        }
+    }
+
+    fun isBanned() = this.isBanned
+
+    fun isDeleted() = this.isDeleted
+
+    fun getEmail() = this.email
+
+    fun toggleBanStatus()  {
+        this.isBanned = !this.isBanned
+    }
+
+    fun toggleDeleteStatus() {
+        this.isDeleted = !this.isDeleted
+    }
+
+    fun changeNickname(request: String) {
+        this.nickname = request
+    }
+
+    fun changePassword(request: String) {
+        this.password = hashPassword(request)
+    }
+
+    fun changeDeletedDate(request: Date) {
+        this.deletedDate = request
+    }
 }
