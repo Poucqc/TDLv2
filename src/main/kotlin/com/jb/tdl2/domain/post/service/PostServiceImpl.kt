f9e396b6b2b462b1449a0663cc132845789bf55d
pcouqc <nagapen@nate.com>
2024-06-29 23:05:39 +0900

    feat : impl getPostList and searchPosts

--- src/main/kotlin/com/jb/tdl2/domain/post/service/PostServiceImpl.kt
+++ src/main/kotlin/com/jb/tdl2/domain/post/service/PostServiceImpl.kt
@@ -6,11 +6,10 @@ import com.jb.tdl2.domain.common.hashtag.HashtagUtils.mergeAndSaveHashtags
 import com.jb.tdl2.domain.exception.NoPermissionException
 import com.jb.tdl2.domain.exception.NotFoundException
 import com.jb.tdl2.domain.post.dto.*
-import com.jb.tdl2.domain.post.model.Like
-import com.jb.tdl2.domain.post.model.LikeId
-import com.jb.tdl2.domain.post.model.Post
+import com.jb.tdl2.domain.post.model.*
 import com.jb.tdl2.domain.post.repository.HashtagRepository
 import com.jb.tdl2.domain.post.repository.LikeRepository
+import com.jb.tdl2.domain.post.repository.PostHashtagRepository
 import com.jb.tdl2.domain.post.repository.postRepository.PostRepository
 import com.jb.tdl2.domain.user.repository.UserRepository
 import jakarta.transaction.Transactional
@@ -24,36 +23,42 @@ class PostServiceImpl(
     private val likeRepository: LikeRepository,
     private val hashtagRepository: HashtagRepository,
     private val commentRepository: CommentRepository,
+    private val postHashtagRepository: PostHashtagRepository
 ) : PostService {
 
     @Transactional
     override fun createPost(currentId: Long, request: UpdatePostRequest): PostResponse {
         val user = userRepository.findByIdOrNull(currentId)
             ?: throw NotFoundException("Not found current user : $currentId")
-        val hashtag = mergeAndSaveHashtags(request.hashtag, hashtagRepository)
-        return postRepository.save(
-            Post(
-                title = request.title,
-                content = request.content,
-                user = user,
-                createdAt = LocalDateTime.now(),
-                updatedAt = LocalDateTime.now(),
-                isBanned = false,
-                isDeleted = false,
-                viewCount = 0,
-                hashtags = hashtag
-            )
-        ).toResponse(comments = emptyList(), likesCount = 0)
+
+        val hashtags = mergeAndSaveHashtags(request.hashtag, hashtagRepository)
+        val post = Post(
+            title = request.title,
+            content = request.content,
+            user = user,
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now(),
+            isBanned = false,
+            isDeleted = false,
+            viewCount = 0
+        )
+        postRepository.save(post)
+        saveHashtagsInPost(hashtags, post)
+
+        return post.toResponse(comments = emptyList(), likesCount = 0, hashtags = hashtags)
     }
 
     @Transactional
     override fun updatePost(postId: Long, currentId: Long, request: UpdatePostRequest, pageable: Pageable): PostResponse {
         val post = postRepository.findByIdOrNull(postId) ?: throw NotFoundException("Post not found")
         if (post.isMyPost(currentId)) throw NoPermissionException("post. postId: $postId")
+
         val hashtags = mergeAndSaveHashtags(request.hashtag, hashtagRepository)
 
-        post.updatePost(request.title, request.content, hashtags)
-        return post.toResponse(comments = findAllComments(postId, pageable), likesCount = countLikes(postId))
+        post.updatePost(request.title, request.content)
+        saveHashtagsInPost(hashtags, post)
+
+        return post.toResponse(comments = findAllComments(postId, pageable), likesCount = countLikes(postId), hashtags)
     }
 
     @Transactional
@@ -63,24 +68,28 @@ class PostServiceImpl(
         post.softDeletePost()
     }
 
-    override fun getPostsList(pageable: Pageable): List<PostListResponse> {
-        // TODO: queryDSL을 사용해 projections contstructor PostListResponse 형태로 pagenation 된 정보 가져오기
-        // TODO: orderby 를 동적으로 만들어서 request 에 해당하는 orderby 적용
-        TODO()
+    override fun getPostsList(pageable: Pageable, cursor: CursorRequest): CursorPageResponse {
+        val postsList = postRepository.getPostList(pageable, cursor)
+
+        val nextCursor = if (postsList.isNotEmpty()) {
+            val lastPost = postsList.last()
+            lastPost.createdAt
+        } else {
+            null
+        }
+
+        return CursorPageResponse(postsList, nextCursor)
     }
 
     override fun getPost(postId: Long, pageable: Pageable): PostResponse {
         val post = postRepository.findByIdOrNull(postId) ?: throw NotFoundException("Post not found")
         if (post.isDeleted()) throw NotFoundException("This post is deleted")
-        return post.toResponse(comments = findAllComments(postId, pageable), likesCount = countLikes(postId))
+        val hashtags = findHashtagsByPostId(postId)
+        return post.toResponse(comments = findAllComments(postId, pageable), likesCount = countLikes(postId), hashtags)
     }
 
-    override fun searchPosts(request: SearchPostRequest, pageable: Pageable): List<PostListResponse> {
-        // TODO: queryDSL을 사용해 projections contstructor PostListResponse 형태로 pagenation 된 정보 가져오기
-        // TODO: search 에 해당하는 builder 를 만들어서 where 조건 적용시키기
-        // TODO: orderby 를 동적으로 만들어서 request 에 해당하는 orderby 적용
-        // TODO: 알고리즘 대신 search에 해당하는 단어가 포함된 횟수를 count() 해서 그 순위별로 orderby 할 수 있을까?
-        TODO()
+    override fun searchPosts(request: SearchPostRequest, pageable: Pageable, orderBy: String): List<PostListResponse> {
+        return postRepository.searchPosts(request, pageable, orderBy)
     }
 
     @Transactional
@@ -106,4 +115,16 @@ class PostServiceImpl(
     private fun likeExist(postId: Long, userId: Long): Boolean {
         return likeRepository.existsById(LikeId(userId, postId))
     }
+
+    private fun findHashtagsByPostId(postId: Long): MutableSet<Hashtag> {
+        return postRepository.findHashtagsByPostId(postId).toMutableSet()
+    }
+
+    private fun saveHashtagsInPost(hashtags: Set<Hashtag>, post: Post) {
+        for (hashtag in hashtags) {
+            val postHashtag = PostHashtag(post = post, hashtag = hashtag)
+            postHashtagRepository.save(postHashtag)
+        }
+    }
+
 }
