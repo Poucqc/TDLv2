c804eaaf6ab6a840f0c00c38899432ff1e6a64ff
pcouqc <nagapen@nate.com>
2024-06-29 23:05:14 +0900

    feat: remove and nullable in search request dto

--- src/main/kotlin/com/jb/tdl2/domain/post/dto/SearchPostRequest.kt
+++ src/main/kotlin/com/jb/tdl2/domain/post/dto/SearchPostRequest.kt
@@ -1,13 +1,11 @@
 package com.jb.tdl2.domain.post.dto
 
-import com.jb.tdl2.domain.post.model.Hashtag
 import java.time.LocalDateTime
 
 data class SearchPostRequest(
-    val keyword: String,
-    val hashtags: Set<Hashtag>,
-    val likesCount: Int,
-    val createdAfter: LocalDateTime
+    val keyword: String?,
+    val likesCount: Int?,
+    val createdAfter: LocalDateTime?,
 )
 
 
